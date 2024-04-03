d3adcea2186f7dff0cde2502b901c0d5dd4983cc
alimacy2508 <70335280+alimacy2508@users.noreply.github.com>
2024-04-03 10:41:38 +0100

    Added the basic javascript to the project
    The project now has the basic javascript for generating a poem set out

--- src/javascript.js
+++ src/javascript.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Your generated poem here",
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+  });
+}
+
+let formElement = document.querySelector("#poem-generator");
+formElement.addEventListener("submit", generatePoem);
