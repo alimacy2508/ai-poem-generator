1dfc5f99151c8f7b864541e2e1355831851da870
alimacy2508 <70335280+alimacy2508@users.noreply.github.com>
2024-04-05 13:07:49 +0100

    Project has been adjusted for travel destinations and fully styled

--- src/javascript.js
+++ src/javascript.js
@@ -1,5 +1,5 @@
-function displayPoem(response) {
-  new Typewriter("#poem", {
+function displayDestination(response) {
+  new Typewriter("#destination", {
     strings: response.data.answer,
     autoStart: true,
     delay: 20,
@@ -7,21 +7,21 @@ function displayPoem(response) {
   });
 }
 
-function generatePoem(event) {
+function generateDestination(event) {
   event.preventDefault();
   let instructions = document.querySelector("#prompt-instructions");
   let apiKey = "bbco0f50t952db4519a3613cfbc4bfb3";
-  let prompt = `User instructions: Generate a Travel poem about ${instructions.value} `;
+  let prompt = `User instructions: Generate a Travel destination that is ${instructions.value} `;
   let context =
-    "You are a romantic poem expert and love to write short poems. You should generate a poem of four lines in BASIC HTML and separate each line with a <br/>. Do not give the poem a title. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong> element AT THE END of the poem and NOT be at the beginning.";
+    "You are a knowledgeable travel expert and love to provide travel destination recommendations. You should generate a travel destination and three short facts about why this destination is good to visit in BASIC HTML and separate each line with a <br/>. You should always have the question of why it would be good to visit the destination before the three short facts, inside a <strong> element. The name of the destination should be the title inside an <h2> element. Make sure to follow the user instructions. Sign the recommendation with 'SheCodes AI' inside a <strong> element AT THE END of the recommendation and NOT be at the beginning. ";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  let poem = document.querySelector("#poem");
-  poem.classList.remove("poem-hidden");
-  poem.innerHTML = `Generating a Travel Poem about ${instructions.value}`;
+  let poem = document.querySelector("#destination");
+  destination.classList.remove("destination-hidden");
+  destination.innerHTML = `Generating a travel destination that is <strong>${instructions.value}</strong>`;
 
-  axios.get(apiUrl).then(displayPoem);
+  axios.get(apiUrl).then(displayDestination);
 }
 
-let formElement = document.querySelector("#poem-generator");
-formElement.addEventListener("submit", generatePoem);
+let formElement = document.querySelector("#destination-generator");
+formElement.addEventListener("submit", generateDestination);
